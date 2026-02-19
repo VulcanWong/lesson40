90f7a4c3306ebffe76fe61cbf7991fd4e72a7a7d
VulcanWong <wongvulcan@gmail.com>
2026-02-19 21:37:35 +0800

    lesson40

--- homework.py
+++ homework.py
@@ -1,10 +1,10 @@
+print("guess a number within 70 to 80")
 num = int(input("Enter a number: "))
-if num > 1:
-    for i in range(2, int(num**0.5) + 1):
-        if num % i == 0:
-            print(f"{num} is not a prime number")
-            break
-        else:
-            print(f"{num} is a prime number")
+if num == 76:
+    print("you are correct")
 else:
-    print(f"{num} is not a prime number")
+    print("wrong")
+if num == 75:
+    print("so close")
+if num == 76:
+    print("so close")
